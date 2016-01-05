ab671213e69042742b9cbfbd6fb296cc83257d8f
Landon Fuller <landonf@plausible.coop>
2016-01-05 15:10:05 -0700

    Explicitly call gmake

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::process::{Command,Stdio};
 
 fn main() {
-    let mut cmd = Command::new("make");
+    let mut cmd = Command::new("gmake");
     cmd.arg("cargo-prep");
     println!("running: {:?}", cmd);
     assert!(cmd.stdin(Stdio::null())
